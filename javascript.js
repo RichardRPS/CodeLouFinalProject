7db7256ee126808307c4786cd60067b3954ba1fc
Richard P Schiller <Rschiller@SBMKYlaw.com>
2017-11-15 19:27:24 -0500

    final project

--- javascript.js
+++ javascript.js
@@ -4,6 +4,19 @@
 var logo = document.getElementById('logo');
 var jsbutton = document.getElementById('jsbutton');
 var text = document.getElementById('text');
+
+var previousColor;
 jsbutton.addEventListener('click', () => {
-    logo.style.color = text.value;
+    // get the current color of the header
+    // assign to previosColor
+    // if currentColor == text.value
+    //  toggle
+    // else 
+    //  set color to text value
+    if(logo.style.color == text.value && previousColor)
+        logo.style.color = previousColor;
+    else {
+        previousColor = logo.style.color;
+        logo.style.color = text.value;
+    }
 });
